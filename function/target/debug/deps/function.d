fcb6177995fce2400c368225291fe27b1a7fe2a1
loshy <loshy244110@gmail.com>
2020-01-18 21:51:33 +0900

    add rust

---
+++ function/target/debug/deps/function.d
@@ -0,0 +1,5 @@
+C:\Users\user\rustprojects\function\target\debug\deps\function.exe: src\main.rs
+
+C:\Users\user\rustprojects\function\target\debug\deps\function.d: src\main.rs
+
+src\main.rs:
---
+++ function/target/debug/function.d
@@ -0,0 +1,1 @@
+C:\Users\user\rustprojects\function\target\debug\function.exe: C:\Users\user\rustprojects\function\src\main.rs
