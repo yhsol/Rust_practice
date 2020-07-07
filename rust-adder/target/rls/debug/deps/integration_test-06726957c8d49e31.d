5c32cd20fef56d357c849d0593c168838948597f
loshy <loshy244110@gmail.com>
2020-07-07 21:51:56 +0900

    feat: using closure

---
+++ rust-adder/target/rls/debug/deps/integration_test-06726957c8d49e31.d
@@ -0,0 +1,6 @@
+c:\Users\user\develop\Rust\rustprojects\rust-adder\target\rls\debug\deps\integration_test-06726957c8d49e31.rmeta: tests\integration_test.rs tests\common\mod.rs
+
+c:\Users\user\develop\Rust\rustprojects\rust-adder\target\rls\debug\deps\integration_test-06726957c8d49e31.d: tests\integration_test.rs tests\common\mod.rs
+
+tests\integration_test.rs:
+tests\common\mod.rs:
