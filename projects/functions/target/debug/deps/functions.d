4f9ca24a81f728bd43a4a23b7fef36ff946174c2
yhsol <loshY244110@gmail.com>
2021-03-08 20:43:42 +0900

    feat: functions

---
+++ projects/functions/target/debug/deps/functions.d
@@ -0,0 +1,5 @@
+C:\Users\user\develop\Rust\rustprojects\projects\functions\target\debug\deps\functions.exe: src\main.rs
+
+C:\Users\user\develop\Rust\rustprojects\projects\functions\target\debug\deps\functions.d: src\main.rs
+
+src\main.rs:
