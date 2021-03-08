552c5ef8fdaebda8ebddf63899433060893ace7e
yhsol <loshY244110@gmail.com>
2021-03-08 20:45:36 +0900

    feat: functions

---
+++ projects/functions/target/rls/debug/deps/functions-57484b8b51d709e8.d
@@ -0,0 +1,5 @@
+c:\Users\user\develop\Rust\rustprojects\projects\functions\target\rls\debug\deps\functions-57484b8b51d709e8.rmeta: src\main.rs
+
+c:\Users\user\develop\Rust\rustprojects\projects\functions\target\rls\debug\deps\functions-57484b8b51d709e8.d: src\main.rs
+
+src\main.rs:
