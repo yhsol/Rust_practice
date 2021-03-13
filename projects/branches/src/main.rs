376ad93316303fa719c436eba977d1fe102a8279
yhsol <loshY244110@gmail.com>
2021-03-13 13:30:56 +0900

    feat: Control Flow

--- projects/branches/src/main.rs
+++ projects/branches/src/main.rs
@@ -1,7 +1,25 @@
 fn main() {
-    let number = 3;
+    using_if_in_let();
+}
 
-    if number != 0 {
-        println!("number was something other than zero");
+fn else_if() {
+    let number = 6;
+
+    if number % 4 == 0 {
+        return println!("number is divisible by 4");
     }
+    if number % 3 == 0 {
+        return println!("number is divisible by 3");
+    }
+    if number % 2 == 0 {
+        return println!("number is divisible by 2");
+    } else {
+        return println!("number is not divisible by 4, 3, or 2");
+    }
+}
+
+fn using_if_in_let() {
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {}", number);
 }
