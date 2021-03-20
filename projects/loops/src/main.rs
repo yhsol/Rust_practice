8e44f8178779e95ec2af14a2b793a5078dfa2e20
yhsol <loshY244110@gmail.com>
2021-03-20 17:34:00 +0900

    feat: loops

--- projects/loops/src/main.rs
+++ projects/loops/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
     // loop_fn();
     // returning_values_from_loops();
-    while_loops();
+    // while_loops();
+    // while_array();
+    // for_iter();
+    for_range();
 }
 
 fn loop_fn() {
@@ -33,5 +36,31 @@ fn while_loops() {
         number -= 1;
     }
 
+    println!("LIFTOFF!!!");
+}
+
+fn while_array() {
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+}
+
+fn for_iter() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element)
+    }
+}
+
+fn for_range() {
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
     println!("LIFTOFF!!!");
 }
