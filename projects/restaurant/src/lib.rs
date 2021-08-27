ffc6a8f1df8cca52882fe6781b8deb33c1937b7e
yhsol <loshY244110@gmail.com>
2021-08-27 19:04:05 +0900

    feat: Managing Growing Projects with Packages, Crates, and Modules

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -6,18 +6,55 @@ mod tests {
     }
 }
 
+fn serve_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     }
+}
 
-    mod serving {
-        fn take_order() {}
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
 
-        fn serve_ordere() {}
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 
-        fn take_payment() {}
-    }
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
