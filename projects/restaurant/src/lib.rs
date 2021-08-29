c29ac651e68b27b8dc29252ee8c4719b3a439da3
yhsol <loshY244110@gmail.com>
2021-08-29 16:37:05 +0900

    feat: control visibility with pub

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -58,3 +58,24 @@ pub fn eat_at_restaurant() {
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
 }
+
+mod outermost {
+    pub fn middle_function() {}
+
+    fn middle_secret_function() {}
+
+    mod inside {
+        pub fn inner_function() {
+            // ::outermost::middle_secret_function();
+        }
+
+        fn secret_function() {}
+    }
+}
+
+fn try_me() {
+    outermost::middle_function();
+    // outermost::middle_secret_function();
+    // outermost::inside::inner_function();
+    // outermost::inside::secret_function();
+}
