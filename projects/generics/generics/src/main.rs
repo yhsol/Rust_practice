1876fb3666cf2c50de5b476cff2e1f18ca4618db
yhsol <loshY244110@gmail.com>
2021-11-09 21:45:51 +0900

    feat: largset fn

--- projects/generics/generics/src/main.rs
+++ projects/generics/generics/src/main.rs
@@ -1,25 +1,23 @@
+fn largest<T>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let mut largest = number_list[0];
+    let result = largest(number_list);
+    println!("The largest number is {}", result);
 
-    for number in number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(char_list);
 
-    println!("The largest number is {}", largest);
-
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-
-    let mut largest = number_list[0];
-
-    for number in number_list {
-        if number > largest {
-            largest = number
-        }
-    }
-
-    println!("The largest number is {}", largest);
+    println!("The largest number is {}", result);
 }
