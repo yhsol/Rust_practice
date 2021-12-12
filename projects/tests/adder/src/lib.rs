1412d8d4d430fee5d5c96e410ca0431a399b2b67
yhsol <loshY244110@gmail.com>
2021-12-12 19:38:59 +0900

    feat: Controlling How Tests Are Run

--- projects/tests/adder/src/lib.rs
+++ projects/tests/adder/src/lib.rs
@@ -1,107 +1,159 @@
-#[derive(Debug)]
-pub struct Rectangle {
-    length: u32,
-    width: u32,
-}
+// #[derive(Debug)]
+// pub struct Rectangle {
+//     length: u32,
+//     width: u32,
+// }
 
-impl Rectangle {
-    pub fn can_hold(&self, other: &Rectangle) -> bool {
-        self.length > other.length && self.width > other.width
-    }
-}
+// impl Rectangle {
+//     pub fn can_hold(&self, other: &Rectangle) -> bool {
+//         self.length > other.length && self.width > other.width
+//     }
+// }
 
-pub struct Guess {
-    value: u32,
-}
+// pub struct Guess {
+//     value: u32,
+// }
 
-impl Guess {
-    pub fn new(value: u32) -> Guess {
-        if value < 1 {
-            panic!(
-                "Guess value must be greater than or equal to 1, get {}.",
-                value
-            );
-        } else if value > 100 {
-            panic!(
-                "Guess value must be less than or equal to 100, got {}.",
-                value
-            )
-        }
+// impl Guess {
+//     pub fn new(value: u32) -> Guess {
+//         if value < 1 {
+//             panic!(
+//                 "Guess value must be greater than or equal to 1, get {}.",
+//                 value
+//             );
+//         } else if value > 100 {
+//             panic!(
+//                 "Guess value must be less than or equal to 100, got {}.",
+//                 value
+//             )
+//         }
 
-        Guess { value }
-    }
-}
+//         Guess { value }
+//     }
+// }
 
 pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
-pub fn greeting(name: &str) -> String {
-    // format!("Hello {}!", name)
-    String::from(name)
-}
+// pub fn greeting(name: &str) -> String {
+//     // format!("Hello {}!", name)
+//     String::from(name)
+// }
+
+// fn prints_and_returns_10(a: i32) -> i32 {
+//     println!("I got the value {}", a);
+//     10
+// }
 
 #[cfg(test)]
 mod tests {
-    #[test] // 이 속성이 바로 이것이 테스트 함수임을 나타냄
-    fn exploration() {
-        assert_eq!(2 + 2, 4);
-    }
+    use super::*; // 위에 정의한 함수들을 사용하기 위해 사용
+
+    // #[test] // 이 속성이 바로 이것이 테스트 함수임을 나타냄
+    // fn exploration() {
+    //     assert_eq!(2 + 2, 4);
+    // }
+
+    // #[test]
+    // fn another() {
+    //     // panic!("Make this test fail")
+    // }
+
+    // #[test]
+    // fn larger_can_hold_smaller() {
+    //     let larger = Rectangle {
+    //         length: 8,
+    //         width: 7,
+    //     };
+    //     let smaller = Rectangle {
+    //         length: 5,
+    //         width: 1,
+    //     };
+
+    //     assert!(larger.can_hold(&smaller))
+    // }
+
+    // #[test]
+    // fn smaller_cannot_hold_larger() {
+    //     let larger = Rectangle {
+    //         length: 8,
+    //         width: 7,
+    //     };
+    //     let smaller = Rectangle {
+    //         length: 5,
+    //         width: 1,
+    //     };
+
+    //     assert!(!smaller.can_hold(&larger))
+    // }
+
+    // #[test]
+    // fn it_adds_two() {
+    //     assert_eq!(4, add_two(2));
+    // }
+
+    // #[test]
+    // fn greeting_contains_name() {
+    //     let result = greeting("Carol");
+    //     // assert!(result.contains("Carol"));
+    //     assert!(
+    //         result.contains("Carol"),
+    //         "Greeting did not contain name, value was `{}`",
+    //         result
+    //     )
+    // }
+
+    // #[test]
+    // #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    // fn greater_than_100() {
+    //     Guess::new(200);
+    // }
+
+    // #[test]
+    // fn it_works() -> Result<(), String> {
+    //     if 2 + 2 == 4 {
+    //         Ok(())
+    //     } else {
+    //         Err(String::from("two plus two does not equal four"))
+    //     }
+    // }
+
+    // #[test]
+    // fn this_test_will_pass() {
+    //     let value = prints_and_returns_10(4);
+    //     assert_eq!(10, value)
+    // }
+
+    // #[test]
+    // fn this_test_will_fail() {
+    //     let value = prints_and_returns_10(8);
+    //     assert_eq!(5, value)
+    // }
 
     #[test]
-    fn another() {
-        // panic!("Make this test fail")
-    }
-
-    use super::*;
-
-    #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            length: 8,
-            width: 7,
-        };
-        let smaller = Rectangle {
-            length: 5,
-            width: 1,
-        };
-
-        assert!(larger.can_hold(&smaller))
-    }
-
-    #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle {
-            length: 8,
-            width: 7,
-        };
-        let smaller = Rectangle {
-            length: 5,
-            width: 1,
-        };
-
-        assert!(!smaller.can_hold(&larger))
-    }
-
-    #[test]
-    fn it_adds_two() {
+    fn add_two_and_two() {
         assert_eq!(4, add_two(2));
     }
 
     #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Carol");
-        // assert!(result.contains("Carol"));
-        assert!(
-            result.contains("Carol"),
-            "Greeting did not contain name, value was `{}`",
-            result
-        )
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
     }
 
     #[test]
-    #[should_panic(expected = "Guess value must be less than or equal to 100")]
-    fn greater_than_100() {
-        Guess::new(200);
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4)
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        // code that takes an hour to run
     }
 }
