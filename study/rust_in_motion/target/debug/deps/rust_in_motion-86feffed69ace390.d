3c173a7b77cfc96f133d3136101010129859f62f
yhsol <loshy244110@gmail.com>
2023-01-24 16:44:39 +0900

    구조 변경

--- study/rust_in_motion/target/debug/deps/rust_in_motion-86feffed69ace390.d
+++
@@ -1,5 +0,0 @@
-/Users/paul/yhsol/rust/rustprojects/projects/rust_in_motion/target/debug/deps/rust_in_motion-86feffed69ace390.rmeta: src/main.rs
-
-/Users/paul/yhsol/rust/rustprojects/projects/rust_in_motion/target/debug/deps/rust_in_motion-86feffed69ace390.d: src/main.rs
-
-src/main.rs:
